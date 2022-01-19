281ff97e089d160b317e07caf7bd825e0120c495
albunus <albunus.nyalita@student.moringaschool.com>
2022-01-19 16:49:40 +0300

    create jobseeker view

--- seekapp/models.py
+++ seekapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+JOBSEEKER_AVAILABILITY = (
+    ('Full Time', "Full Time"),
+    ('Part Time', "Part Time"),
+)
+
+class User(AbstractUser):
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []#removes email from REQUIRED_FIELDS
+    is_admin = models.BooleanField(default=False)
--- seekapp/views.py
+++ seekapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from seekapp.models import *
+from django.shortcuts import render
+
 
 # Create your views here.
+
+
+
+def profile_jobseeker(request):
+  current_user = request.user
+  return render(request,'#',{"current_user":current_user})
+
+
