aa62795ddbbd1168d4f29b76f92ff6fc7223d2cb
bre-nda <brenda.kanale@student.moringaschool.com>
2022-01-19 21:44:31 +0300

    update changes in the views and urls

--- seekapp/views.py
+++ seekapp/views.py
@@ -2,10 +2,6 @@ from django.shortcuts import render
 
 
 # Create your views here.
-def index(request):
-    return render(request,'jobseeker/index.html')
-
-
 def profile(request):
     return render(request, "jobseeker/profile.html", {"profile": profile,})
 
