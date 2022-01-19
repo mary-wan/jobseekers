224a7f80d89eaa7b8b108bb421cfd63eda81d19a
anipherchelsea <chelsea.ayoo@student.moringaschool.com>
2022-01-19 06:12:13 -0500

    add register url to url patterns

--- seekapp/urls.py
+++ seekapp/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
+    path('register/', views.register, name='register'),
    
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
