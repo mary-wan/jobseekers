fcfdc1c90daf0154fd1d64ae3856ade141da702d
albunus <albunus.nyalita@student.moringaschool.com>
2022-01-19 16:50:06 +0300

    create employer view

--- seekapp/views.py
+++ seekapp/views.py
@@ -11,4 +11,12 @@ def profile_jobseeker(request):
   current_user = request.user
   return render(request,'#',{"current_user":current_user})
 
+def profile_employer(request):
+    employer=request.user
+    available=User.objects.filter(is_jobseeker= True,verified=True).all() 
+    context={
+        "employer":employer,
+        "available":available,
+    }
+    return render(request,'#',context)
 
